ed6df5150107ed30ec3abc62b2bd94bfc479d77e
lonky <yan.vashchenko@gmail.com>
2026-08-25 22:20:07 +0100

    first lesson and commit

---
+++ course.ts
@@ -0,0 +1,1 @@
+export const course: string = "Автоматизации тестирования на JS/TS";
---
+++ index.ts
@@ -0,0 +1,5 @@
+import { name } from "./name.js"
+import { course } from "./course.js"
+import { age } from "./age.js"
+
+console.log(`Привет, меня зовут ${name}, я прохожу курс по ${course}, мне ${age}.`);
