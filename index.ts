4d99ecfae4c161449b9fa03fcea181d2b1c44b96
Yan Vashchanka <yan.vashchenko@gmail.com>
2026-08-28 10:08:57 +0300

    feat: add prettier

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
-import { name } from "./name"
-import { course } from "./course"
-import { age } from "./age"
+import { name } from "./name";
+import { course } from "./course";
+import { age } from "./age";
 
 console.log(`Привет, меня зовут ${name}, я прохожу курс по ${course}, мне ${age}.`);
